8a3eda1ae515340cce6018719d3518b2052d3d05
Jaewoopudding <ijnn258@gmail.com>
2023-06-24 23:34:23 +0900

    cache delete

--- utils/model_trainer.py
+++ utils/model_trainer.py
@@ -1,11 +1,15 @@
 import tqdm
+import torch
 
 
 def autoencoder_trainer(model, criteria, optimizer, train_loader,
                         test_loader, device, epochs):
-    train_loss = []
-    test_loss = []
+    train_loss_hist = []
+    test_loss_hist = []
+    
     for epoch in tqdm.tqdm(range(epochs)):
+        train_loss = []
+        test_loss = []
         for x, _ in train_loader:
             x = x.to(device)
             model.train()
@@ -15,23 +19,28 @@ def autoencoder_trainer(model, criteria, optimizer, train_loader,
             loss.backward()
             optimizer.step()
             train_loss.append(loss.item())
-        
+        train_loss_hist.append(torch.tensor(train_loss).mean())
         model.eval()
         for x, _ in test_loader:
             x = x.to(device)
             _, reconstruct = model(x)
             loss = criteria(reconstruct, x)
             test_loss.append(loss.item())
-            
-        print(f'epochs: {epoch + 1} - Train loss: {train_loss[-1]} - Test loss: {test_loss[-1]}')
-    return train_loss, test_loss
+        train_loss_hist.append(torch.tensor(test_loss).mean())
+        if epoch % 10 == 0:    
+            print(f'epochs: {epoch + 1} - Train loss: {train_loss[-1]} - Test loss: {test_loss[-1]}')
+    return train_loss_hist, test_loss_hist
 
 
 def vae_trainer(model, criteria, optimizer, train_loader,
                 test_loader, device, epochs, beta=1):
-    train_loss = []
-    test_loss = []
+    
+    train_loss_hist = []
+    test_loss_hist = []
+    
     for epoch in tqdm.tqdm(range(epochs)):
+        train_loss = []
+        test_loss = []
         for x, _ in train_loader:
             x = x.to(device)
             model.train()
@@ -44,6 +53,7 @@ def vae_trainer(model, criteria, optimizer, train_loader,
             loss.backward()
             optimizer.step()
             train_loss.append(loss.item())
+        train_loss_hist.append(torch.tensor(train_loss).mean())
         
         model.eval()
         for x, _ in test_loader:
@@ -54,7 +64,9 @@ def vae_trainer(model, criteria, optimizer, train_loader,
             kl_loss = kl_loss / x.shape[0]
             loss = reconstruction_loss + beta * kl_loss
             test_loss.append(loss.item())
-            
-        print(f'epochs: {epoch + 1} - Train loss: {train_loss[-1]} - Test loss: {test_loss[-1]}')
-        print(f'Test Error Ratio || reconstruction error: {reconstruction_loss} , KLD: {kl_loss}')
-    return train_loss, test_loss
+        test_loss_hist.append(torch.tensor(test_loss).mean())
+        
+        if epoch % 10 == 0:     
+            print(f'epochs: {epoch + 1} - Train loss: {train_loss[-1]} - Test loss: {test_loss[-1]}')
+            print(f'Test Error Ratio || reconstruction error: {reconstruction_loss} , KLD: {kl_loss}')
+    return train_loss_hist, test_loss_hist
--- utils/visualization.py
+++ utils/visualization.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 def visualization(loader, model, device, num_of_samples=5):
     for x, _ in loader:
         recon = model(x.to(device))[-1]
-        fig, ax = plt.subplots(num_of_samples, 2, figsize=(4, 10))
-        plt.suptitle("Left : Original, Right : Reconstructed")
+        fig, ax = plt.subplots(2, num_of_samples, figsize=(10, 4))
+        plt.suptitle("Up : Original, Down : Reconstructed")
         for i in range(num_of_samples):
-            ax[i, 0].imshow(x[i].view(28, 28).detach().numpy(), cmap='gray')
-            ax[i, 1].imshow(recon[i].cpu().view(28, 28).detach().numpy(), cmap='gray')
+            ax[0, i].imshow(x[i].view(28, 28).detach().numpy(), cmap='gray')
+            ax[1, i].imshow(recon[i].cpu().view(28, 28).detach().numpy(), cmap='gray')
         plt.show()
         break
     
